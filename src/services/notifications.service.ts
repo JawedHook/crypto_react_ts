7b547fdd7268d659fd4528f52e5204e0dc023cf4
Thomas Leveque <thomasl5895@gmail.com>
2020-01-22 14:13:16 +0100

    upodate notifService from mapping to export function

--- src/services/notifications.service.ts
+++ src/services/notifications.service.ts
@@ -7,57 +7,55 @@ import axios from 'axios';
 
 const NOTIFICATION_URL = 'https://exp.host/--/api/v2/push/send';
 
-const notificationsService = {
-  checkNotificationsPerm: async (currentUser: User) => {
-    try {
-      // Check notification permission
-      const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
-      let final = status;
+const checkNotificationsPerm = async (currentUser: User) => {
+  try {
+    // Check notification permission
+    const { status } = await Permissions.getAsync(Permissions.NOTIFICATIONS);
+    let final = status;
 
-      // If not allowed, ask for permission
-      if (status !== 'granted') {
-        const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
-        final = status;
-      }
+    // If not allowed, ask for permission
+    if (status !== 'granted') {
+      const { status } = await Permissions.askAsync(Permissions.NOTIFICATIONS);
+      final = status;
+    }
 
-      // If not granted, exit function
-      if (final !== 'granted') {
-        await this.removePhoneToken(currentUser.id);
-        return;
-      }
-      await this.setPhoneToken(currentUser.id);
-    } catch (err) {
-      console.log('checkNotificationsPerm: ', err.message || err.toString());
+    // If not granted, exit function
+    if (final !== 'granted') {
+      await removePhoneToken(currentUser.id);
+      return;
     }
-  },
-  setPhoneToken: async (currentUserId: string) => {
-    try {
-      let phoneToken = await Notifications.getExpoPushTokenAsync();
-      await firestore.doc(`users/${currentUserId}`).update({ phoneToken });
-      await setStoragePhoneToken(phoneToken);
-    } catch (err) {
-      console.log('setPhoneToken: ', err.message || err.toString());
-    }
-  },
-  removePhoneToken: async (currentUserId: string) => {
-    try {
-      await firestore.doc(`users/${currentUserId}`).update({ phoneToken: null });
-      await removeStoragePhoneToken();
-    } catch (err) {
-      console.log('removePhoneToken: ', err.message || err.toString());
-    }
-  },
-  sendNotification: async (currentUser: User, { title, body }) => {
-    try {
-      axios.post(NOTIFICATION_URL, {
-        to: currentUser.phoneToken,
-        title,
-        body,
-      });
-    } catch (err) {
-      console.log('sendNotification: ', err);
-    }
-  },
+    await setPhoneToken(currentUser.id);
+  } catch (err) {
+    console.log('checkNotificationsPerm: ', err.message || err.toString());
+  }
+};
+const setPhoneToken = async (currentUserId: string) => {
+  try {
+    let phoneToken = await Notifications.getExpoPushTokenAsync();
+    await firestore.doc(`users/${currentUserId}`).update({ phoneToken });
+    await setStoragePhoneToken(phoneToken);
+  } catch (err) {
+    console.log('setPhoneToken: ', err.message || err.toString());
+  }
+};
+const removePhoneToken = async (currentUserId: string) => {
+  try {
+    await firestore.doc(`users/${currentUserId}`).update({ phoneToken: null });
+    await removeStoragePhoneToken();
+  } catch (err) {
+    console.log('removePhoneToken: ', err.message || err.toString());
+  }
+};
+const sendNotification = async (currentUser: User, { title, body }) => {
+  try {
+    axios.post(NOTIFICATION_URL, {
+      to: currentUser.phoneToken,
+      title,
+      body,
+    });
+  } catch (err) {
+    console.log('sendNotification: ', err);
+  }
 };
 
-export default notificationsService;
+export default { checkNotificationsPerm, setPhoneToken, removePhoneToken, sendNotification };
